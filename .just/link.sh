fc281c7eee7fab3f0cc617a0cbd0ddc33b88c44a
DanielRivasMD <danielrivasmd@gmail.com>
2024-10-23 06:44:13 -0600

    update link

--- .just/link.sh
+++ .just/link.sh
@@ -1,1 +1,13 @@
-ln -sf "${karabiner}" "${config}/"                                             && echo "Linked ${BIYELLOW}=>${NC} ${BBLUE}karabiner${NC}"
+#!/bin/bash
+####################################################################################################
+
+# config
+source ".config/config.sh"
+
+####################################################################################################
+
+# link
+ln -sf "${karabiner}" "${HOME}/.config/"         && echo "Linked ${BIYELLOW}=>${NC} ${BBLUE}karabiner${NC}"
+ln -sf "${karabiner}/karabiner.edn" "${config}/" && echo "Linked ${BIYELLOW}=>${NC} ${BBLUE}karabiner${NC}"
+
+####################################################################################################
