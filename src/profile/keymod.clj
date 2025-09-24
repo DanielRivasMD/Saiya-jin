2405794d2ddaa5e84e982a0fd648b793278384e4
Daniel Rivas <danielrivasmd@gmail.com>
2025-09-24 14:54:49 +0200

    refactor keymod

---
+++ src/profile/keymod.clj
@@ -0,0 +1,60 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; KEY MOD
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(ns profile.keymod
+  (:require [clojure.java.io :as io]
+            [clojure.pprint :as pp]
+            [config :as c]))
+
+(def out-file "keymod.edn")
+
+(defn keymod []
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+; TODO: update
+; key modifiers
+  {:des "Modifier key remappings"
+   :rules
+   [; quit
+    [:!C#Pq [:!Cq ["command-q" 0]] ["command-q" 1]]                                                  ;
+    [:!C#Pq ["command-q" 1] nil {:delayed {:invoked ["command-q" 0] :canceled ["commandq" 0]}}]      ;
+
+  ; esc
+  ; TODO: CHANGE `term` pop-up new
+    [:#Pescape :!EWright_shift nil {:alone [:!EW#Pnon_us_pound]}]                                    ; menu search.
+
+  ; zero
+    [:#Pkeypad_num_lock :!EQright_shift nil {:alone [:keypad_num_lock]}]                             ; mouse control
+
+  ; launcher
+    [:#Ptab :!OTCright_shift nil {:alone :tab}]                                                      ; spectacle control, speak & app launcher
+
+  ; joker
+    [:##caps_lock :!EWright_command nil {:alone :escape}]                                            ; esc, language server protocol
+
+  ; grave
+  ; [:#Pright_control :right_control nil {:alone [:!W#Pnon_us_pound]}]                               ; arc bind
+
+  ; lefts
+    [:#Pleft_control :left_control nil {:alone [:!EWQ#Onon_us_pound]}]                               ; alfred window switcher
+    [:#Pleft_option :left_option nil {:alone [:!Otab]}]                                              ; global in-app window cycler.
+    [:#Pleft_command :left_command nil {:alone [:!Ctab]}]                                            ; switch most recent app
+
+  ; rights
+    [:#Pright_shift :right_shift nil {:alone [:!R#Pnon_us_pound]}]                                   ; `browser` input. `term` history edit
+    [:#Pright_option :right_option nil {:alone [:!E#Pnon_us_pound]}]                                 ; alfred paste
+    [:#Pright_command :right_command nil {:alone [:!Q#Pnon_us_pound]}]                               ; alfred clipboard select
+    ]})
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defn -main []
+  (with-open [w (io/writer (str c/edn-path out-file))]
+    (binding [*out* w
+              *print-meta* true
+              *print-namespace-maps* false]
+      (pp/pprint (keymod)))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
