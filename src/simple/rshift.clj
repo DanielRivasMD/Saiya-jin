2070261e92b5358499a6307031b75819f809abeb
Daniel Rivas <danielrivasmd@gmail.com>
2025-09-24 14:56:02 +0200

    refactor shift

---
+++ src/simple/rshift.clj
@@ -0,0 +1,46 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; SHIFT
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(ns simple.rshift
+  (:require [clojure.java.io :as io]
+            [clojure.pprint :as pp]
+            [config :as c]))
+
+(def out-file "rshift.edn")
+
+(defn rshift []
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+  {:des "Shift Mode"
+   :rules
+   [;
+    [:#Pright_shift :right_shift [:term] {:alone [:!Oa]}]                                            ; history term
+  ; [:!R#Pleft_arrow []]
+  ; [:!R#Pright_arrow []]
+  ; [:!R#Pup_arrow []]
+  ; [:!R#Pdown_arrow []]
+
+    ^{:doc/actions [{:action "delete line start", :exec ["kill_to_line_start"], :program "helix-common"}
+                    {:action "delete line start", :exec ["SelectToStartOfLine,Delete"], :program "micro"}]} [:!R#Pleft_command [:!Wu]]
+  ; [:!Rleft_option []]
+    ^{:doc/actions  [{:action "delete prev word", :exec ["delete_word_backward"], :program "helix-common"}
+                     {:action "delete prev word", :exec ["DeleteWordLeft"], :program "micro"}]} [:!R#Pleft_control [:!Ww]]
+    ^{:doc/actions [{:action "delete prev char", :exec ["delete_char_backward"], :program "helix-common"}]} [:!R#Pleft_shift [:delete_or_backspace]]                                                         ;
+  ; [:!R#Pcaps_lock []]
+  ; [:!R#Ptab []]
+  ; [:!R#Pkeypad_num_lock []]
+  ; [:!R#Pescape []]
+    ]})
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defn -main []
+  (with-open [w (io/writer (str c/edn-path out-file))]
+    (binding [*out* w
+              *print-meta* true
+              *print-namespace-maps* false]
+      (pp/pprint (rshift)))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
