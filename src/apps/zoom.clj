eb9732fb7181b3b4ca1adfccaab2658710fa4a8a
Daniel Rivas <danielrivasmd@gmail.com>
2025-09-24 14:54:04 +0200

    refactor zoom

---
+++ src/apps/zoom.clj
@@ -0,0 +1,41 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; zoom
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(ns apps.zoom
+  (:require [clojure.java.io :as io]
+            [clojure.pprint :as pp]
+            [config :as c]))
+
+(def out-file "zoom.edn")
+
+(defn zoom []
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+; TODO: homogenize to format
+; zoom
+  {:des ""
+   :rules
+   [;
+  ; mute / unmute
+    [:!C#Pspacebar :!SC#Pa [:zoom]]                                                                  ;
+
+  ; camera on / off
+    [:!T#Pspacebar :!SC#Pv [:zoom]]                                                                  ;
+
+  ; text show
+    [:#Pright_shift :right_shift [:zoom] {:alone [:!SC#Ph]}]                                         ;
+    ]})
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defn -main []
+  (with-open [w (io/writer (str c/edn-path out-file))]
+    (binding [*out* w
+              *print-meta* true
+              *print-namespace-maps* false]
+      (pp/pprint (zoom)))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
