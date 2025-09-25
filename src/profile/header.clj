6ebbc12e7992b81090d5e3c7f4c41a0584174f93
Daniel Rivas <danielrivasmd@gmail.com>
2025-09-25 06:19:03 +0200

    update header

--- src/profile/header.clj
+++ src/profile/header.clj
@@ -17,7 +17,6 @@
 ;; !! | mandatory command + control + optional + shift (hyper)
 ;; ## | optional any
 
-; BUG: identify! issue resides on the brackets closing up as a unit over Default, to resolve, segregate each block & join them by echoed text
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (ns profile.header
@@ -27,50 +26,16 @@
 
 (def out-file "header.edn")
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (def header
-[
-  {:Default
-   {:default true
-    :sim     50    ;; simultaneous_threshold_milliseconds (def: 50)
-    :delay   300   ;; to_delayed_action_delay_milliseconds (def: 500)
-    :alone   300   ;; to_if_alone_timeout_milliseconds (def: 1000)
-    :held    500}  ;; to_if_held_down_threshold_milliseconds (def: 500)
-}
-
-   {:applications
-   {:browser ["company.thebrowser.Browser"]
-    :finder  ["com.apple.finder"]
-    :mail    ["io.canarymail.mac"]
-    :skim    ["net.sourceforge.skim-app.skim"]
-    :term    ["org.alacritty"]
-    :zoom    ["us.zoom.xos"]}
-}
-
-{:devices
-   {:pro    [{:vendor_id 1452 :product_id 832}]
-    :air    [{:vendor_id 1452 :product_id 641}]
-    :master [{:vendor_id 1133 :product_id 45915}]}
-}
-
-   {:templates
-   {:launch "osascript -e 'tell application \"%s\" to activate'"}
-}
-   {:layers
-   {:zero-mode      {:key :keypad_num_lock}
-    :q-mode         {:key :q}
-    :w-mode         {:key :w}
-    :j-mode         {:key :j}
-    :k-mode         {:key :k}
-    :z-mode         {:key :z}
-    :x-mode         {:key :x}
-    :obracket-mode  {:key :open_bracket}   ;; '[' + letter = rust snippet
-    :cbracket-mode  {:key :close_bracket}  ;; ']' + letter = go snippet
-    :semicolon-mode {:key :semicolon}      ;; ';' + letter = umlaut
-    :quote-mode     {:key :quote}          ;; ''' + letter = circumflex
-    :backslash-mode {:key :backslash}      ;; '\' + letter = grave accent
-    :comma-mode     {:key :comma}
-    :period-mode    {:key :period}
-    :slash-mode     {:key :slash}}}])
+  [{:Default
+    {:default true
+     :sim     50    ;; simultaneous_threshold_milliseconds (def: 50)
+     :delay   300   ;; to_delayed_action_delay_milliseconds (def: 500)
+     :alone   300   ;; to_if_alone_timeout_milliseconds (def: 1000)
+     :held    500  ;; to_if_held_down_threshold_milliseconds (def: 500)
+     }}])
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
