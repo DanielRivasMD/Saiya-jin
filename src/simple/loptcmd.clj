775c7e4eadc2213f3489def48eccc0d951095ef0
Daniel Rivas <danielrivasmd@gmail.com>
2025-09-26 11:38:30 +0200

    update alt cmd

--- src/simple/loptcmd.clj
+++ src/simple/loptcmd.clj
@@ -9,6 +9,9 @@
 
 (def out-file "loptcmd.edn")
 
+(def zj-rename-tab-mode ["SwitchToMode \"RenameTab\"; TabNameInput 0;"])
+(def zt-abort-rename ["UndoRenameTab; SwitchToMode \"Normal\";"])
+
 (defn loptcmd []
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -17,19 +20,12 @@
    :rules
    [;
   ; arrow glyphs
-    ^{:doc/actions [{:program "zellij", :action "jump prev buffer", :exec ["GoToPreviousTab;"]}]}
-    [:!OC#Pleft_arrow          [:!SOj]             :term]
-
-    ^{:doc/actions [{:program "zellij", :action "jump next buffer", :exec ["GoToNextTab;"]}]}
-    [:!OC#Pright_arrow         [:!SOk]             :term]
-
+    ^{:doc/actions [{:program c/zj, :action "jump prev buffer", :exec ["GoToPreviousTab;"]}]}    [:!OC#Pleft_arrow          [:!SOj]             :term]
+    ^{:doc/actions [{:program c/zj, :action "jump next buffer", :exec ["GoToNextTab;"]}]}    [:!OC#Pright_arrow         [:!SOk]             :term]
     ^{:doc/actions [{:action "scroll up", :exec ["scroll_up"], :program "helix-common"}
-                    {:action "scroll up", :exec ["scrollUpMain-alt2"], :program "lazygit"}]}
-    [:!OC#Pup_arrow            [:!Tx]              :term]
-
+                    {:action "scroll up", :exec ["scrollUpMain-alt2"], :program "lazygit"}]}    [:!OC#Pup_arrow            [:!Tx]              :term]
     ^{:doc/actions [{:action "scroll down", :exec ["scroll_down"], :program "helix-common"}
-                    {:action "scroll down", :exec ["scrollDownMain-alt2"], :program "lazygit"}]}
-    [:!OC#Pdown_arrow          [:!Ty]              :term]
+                    {:action "scroll down", :exec ["scrollDownMain-alt2"], :program "lazygit"}]}    [:!OC#Pdown_arrow          [:!Ty]              :term]
 
     ^{:doc/actions [{}]} [:!OCS#Pleft_arrow  [:!OCSleft_arrow]  [:term]]
     ^{:doc/actions [{}]} [:!OCS#Pright_arrow [:!OCSright_arrow] [:term]]
@@ -37,9 +33,9 @@
     ^{:doc/actions [{}]} [:!OCS#Pdown_arrow  [:!OCSdown_arrow]  [:term]]
 
   ; technical glyphs
-    ^{:doc/actions [{:action "tab sync",  :exec ["ToggleActiveSyncTab;"], :program "zellij"}]}    [:!OC#Popen_bracket  [:!SOv] [:term]]
-    ^{:doc/actions [{:action "plugin jump-list",  :exec ["LaunchOrFocusPlugin \"file:~/.config/zellij/plugins/zellij-jump-list.wasm\" { floating true; move_to_focused_tab true; };"], :program "zellij"}]}    [:!OC#Pclose_bracket [:!SOw] [:term]]
-    ^{:doc/actions [{:action "plugin monocle",   :exec ["LaunchOrFocusPlugin \"file:~/.config/zellij/plugins/monocle.wasm\" { floating true; }; SwitchToMode \"Normal\";"], :program "zellij"}]}    [:!OC#Psemicolon     [:!SOx] [:term]]
+    ^{:doc/actions [{:action "tab sync",  :exec ["ToggleActiveSyncTab;"], :program c/zj}]}    [:!OC#Popen_bracket  [:!SOv] [:term]]
+    ^{:doc/actions [{:action "plugin jump-list",  :exec ["LaunchOrFocusPlugin \"file:~/.config/zellij/plugins/zellij-jump-list.wasm\" { floating true; move_to_focused_tab true; };"], :program c/zj}]}    [:!OC#Pclose_bracket [:!SOw] [:term]]
+    ^{:doc/actions [{:action "plugin monocle",   :exec ["LaunchOrFocusPlugin \"file:~/.config/zellij/plugins/monocle.wasm\" { floating true; }; SwitchToMode \"Normal\";"], :program c/zj}]}    [:!OC#Psemicolon     [:!SOx] [:term]]
     ^{:doc/actions [{}]}    [:!OC#Pquote         [:!SOy] [:term]]
     ^{:doc/actions [{}]}    [:!OC#Pbackslash     [:!SOz] [:term]]
     ^{:doc/actions [{:action "nushell motion",   :exec [], :program "alacritty"}]} [:!OC#Pcomma         [:!Ox]  [:term]]
@@ -56,12 +52,13 @@
     ^{:doc/actions [{}]} [:!OCS#Pslash          [:!OCSslash]]
 
   ; action glyphs
-    ^{:doc/actions [{:action "tab close",     :exec ["CloseTab;"], :program "zellij"}]} [:!OC#Pdelete_or_backspace [:!SOu] [:term]]
-    ^{:doc/actions [{:action "plugin room",      :exec ["LaunchOrFocusPlugin \"file:~/.config/zellij/plugins/room.wasm\" { floating true; ignore_case true; };"], :program "zellij"}]} [:!OC#Preturn_or_enter     [:!SOp] [:term]]
-    ^{:doc/actions [{:action "tab new",       :exec ["NewTab;"], :program "zellij"}]} [:!OC#Pright_shift         [:!SOq] [:term]]
-    ^{:doc/actions [{:action "tab break pane",:exec ["BreakPane;"], :program "zellij"}]} [:!OC#Pright_option        [:!SOr] [:term]]
-    ^{:doc/actions [{:action "tab rename",    :exec ["SwitchToMode \"RenameTab\"; TabNameInput 0;"], :program "zellij"}]} [:!OC#Pright_command       [:!SOs] [:term]]
-    ^{:doc/actions [{:action "tab jump back", :exec ["ToggleTab;"], :program "zellij"}]} [:!OC#Pspacebar            [:!SOt] [:term]]
+    ^{:doc/actions [{:action "tab close",     :exec ["CloseTab;"], :program c/zj}]} [:!OC#Pdelete_or_backspace [:!SOu] [:term]]
+    ^{:doc/actions [{:action "plugin room",      :exec ["LaunchOrFocusPlugin \"file:~/.config/zellij/plugins/room.wasm\" { floating true; ignore_case true; };"], :program c/zj}]} [:!OC#Preturn_or_enter     [:!SOp] [:term]]
+    ^{:doc/actions [{:action "tab new",       :exec ["NewTab;"], :program c/zj}]} [:!OC#Pright_shift         [:!SOq] [:term]]
+    ^{:doc/actions [{:action "tab break pane",:exec ["BreakPane;"], :program c/zj}]} [:!OC#Pright_option        [:!SOr] [:term]]
+    ^{:doc/actions [{:program c/zj,    :action "mode tab rename",    :exec zj-rename-tab-mode}
+                    {:program c/zt,    :action "abort tab reanme",   :exec zt-abort-rename}]}      [:!OC#Pright_command  [:!SOs] :term]
+    ^{:doc/actions [{:action "tab jump back", :exec ["ToggleTab;"], :program c/zj}]} [:!OC#Pspacebar            [:!SOt] [:term]]
 
     ^{:doc/actions [{}]} [:!OCS#Pdelete_or_backspace [:!OCSdelete_or_backspace]]
     ^{:doc/actions [{}]} [:!OCS#Preturn_or_enter     [:!OCSreturn_or_enter]]
@@ -71,16 +68,16 @@
     ^{:doc/actions [{}]} [:!OCS#Pspacebar            [:!OCSspacebar]]
 
   ; numeric glyphs
-    ^{:doc/actions [{:action "alt-cmd-1", :exec ["SwitchToMode \"Tab\"; GoToTab 1; SwitchToMode \"Normal\";"], :program "zellij"}]} [:!OC#P1 [:!SOa] [:term]]
-    ^{:doc/actions [{:action "alt-cmd-2", :exec ["SwitchToMode \"Tab\"; GoToTab 2; SwitchToMode \"Normal\";"], :program "zellij"}]} [:!OC#P2 [:!SOb] [:term]]
-    ^{:doc/actions [{:action "alt-cmd-3", :exec ["SwitchToMode \"Tab\"; GoToTab 3; SwitchToMode \"Normal\";"], :program "zellij"}]} [:!OC#P3 [:!SOc] [:term]]
-    ^{:doc/actions [{:action "alt-cmd-4", :exec ["SwitchToMode \"Tab\"; GoToTab 4; SwitchToMode \"Normal\";"], :program "zellij"}]} [:!OC#P4 [:!SOd] [:term]]
-    ^{:doc/actions [{:action "alt-cmd-5", :exec ["SwitchToMode \"Tab\"; GoToTab 5; SwitchToMode \"Normal\";"], :program "zellij"}]} [:!OC#P5 [:!SOe] [:term]]
-    ^{:doc/actions [{:action "alt-cmd-6", :exec ["SwitchToMode \"Tab\"; GoToTab 6; SwitchToMode \"Normal\";"], :program "zellij"}]} [:!OC#P6 [:!SOf] [:term]]
-    ^{:doc/actions [{:action "alt-cmd-7", :exec ["SwitchToMode \"Tab\"; GoToTab 7; SwitchToMode \"Normal\";"], :program "zellij"}]} [:!OC#P7 [:!SOg] [:term]]
-    ^{:doc/actions [{:action "alt-cmd-8", :exec ["SwitchToMode \"Tab\"; GoToTab 8; SwitchToMode \"Normal\";"], :program "zellij"}]} [:!OC#P8 [:!SOh] [:term]]
-    ^{:doc/actions [{:action "alt-cmd-9", :exec ["SwitchToMode \"Tab\"; GoToTab 9; SwitchToMode \"Normal\";"], :program "zellij"}]} [:!OC#P9 [:!SOi] [:term]]
-    ^{:doc/actions [{:action "alt-cmd-0", :exec ["GoToPreviousTab;"], :program "zellij"}]} [:!OC#P0 [:!SOj] [:term]]
+    ^{:doc/actions [{:action "alt-cmd-1", :exec ["SwitchToMode \"Tab\"; GoToTab 1; SwitchToMode \"Normal\";"], :program c/zj}]} [:!OC#P1 [:!SOa] [:term]]
+    ^{:doc/actions [{:action "alt-cmd-2", :exec ["SwitchToMode \"Tab\"; GoToTab 2; SwitchToMode \"Normal\";"], :program c/zj}]} [:!OC#P2 [:!SOb] [:term]]
+    ^{:doc/actions [{:action "alt-cmd-3", :exec ["SwitchToMode \"Tab\"; GoToTab 3; SwitchToMode \"Normal\";"], :program c/zj}]} [:!OC#P3 [:!SOc] [:term]]
+    ^{:doc/actions [{:action "alt-cmd-4", :exec ["SwitchToMode \"Tab\"; GoToTab 4; SwitchToMode \"Normal\";"], :program c/zj}]} [:!OC#P4 [:!SOd] [:term]]
+    ^{:doc/actions [{:action "alt-cmd-5", :exec ["SwitchToMode \"Tab\"; GoToTab 5; SwitchToMode \"Normal\";"], :program c/zj}]} [:!OC#P5 [:!SOe] [:term]]
+    ^{:doc/actions [{:action "alt-cmd-6", :exec ["SwitchToMode \"Tab\"; GoToTab 6; SwitchToMode \"Normal\";"], :program c/zj}]} [:!OC#P6 [:!SOf] [:term]]
+    ^{:doc/actions [{:action "alt-cmd-7", :exec ["SwitchToMode \"Tab\"; GoToTab 7; SwitchToMode \"Normal\";"], :program c/zj}]} [:!OC#P7 [:!SOg] [:term]]
+    ^{:doc/actions [{:action "alt-cmd-8", :exec ["SwitchToMode \"Tab\"; GoToTab 8; SwitchToMode \"Normal\";"], :program c/zj}]} [:!OC#P8 [:!SOh] [:term]]
+    ^{:doc/actions [{:action "alt-cmd-9", :exec ["SwitchToMode \"Tab\"; GoToTab 9; SwitchToMode \"Normal\";"], :program c/zj}]} [:!OC#P9 [:!SOi] [:term]]
+    ^{:doc/actions [{:action "alt-cmd-0", :exec ["GoToPreviousTab;"], :program c/zj}]} [:!OC#P0 [:!SOj] [:term]]
     ^{:doc/actions [{}]} [:!OC#P0 [:!OC0]]
     ^{:doc/actions [{}]} [:!OC#Phyphen [:!OChyphen]]
     ^{:doc/actions [{}]} [:!OC#Pequal_sign [:!OCequal_sign]]
