57d205af246eede8135adb5801ac2575b689a4df
Daniel Rivas <danielrivasmd@gmail.com>
2025-09-29 07:20:25 +0200

    update keymod

--- src/profile/keymod.clj
+++ src/profile/keymod.clj
@@ -9,54 +9,54 @@
 
 (def out-file "keymod.edn")
 
-(def hi-normal ["normal_mode"])
-(def hn-insert ["insert_mode"])
-(def hs-normal ["normal_mode"])
+(def hc-collapse             ["collapse_selection", "keep_primary_selection"])
+(def hi-normal               ["normal_mode"])
+(def hn-insert               ["insert_mode"])
+(def hs-normal               ["normal_mode"])
+(def zp-escape               ["UndoRenamePane; SwitchToMode \"Normal\";"])
+(def zt-escape               ["UndoRenameTab; SwitchToMode \"Normal\";"])
+(def zx-escape               ["SwitchToMode \"Normal\";"])
 
 (defn keymod []
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-; TODO: update
-; key modifiers
+  ; key modifiers
   {:des "Modifier key remappings"
    :rules
    [; quit
-    [:!C#Pq [:!Cq ["command-q" 0]] ["command-q" 1]]                                                  ;
-    [:!C#Pq ["command-q" 1] nil {:delayed {:invoked ["command-q" 0] :canceled ["commandq" 0]}}]      ;
+    [:!C#Pq [:!Cq ["command-q" 0]] ["command-q" 1]]
+    [:!C#Pq ["command-q" 1] nil {:delayed {:invoked ["command-q" 0] :canceled ["commandq" 0]}}]
 
-  ; esc
-  ; TODO: CHANGE `term` pop-up new
-    [:#Pescape :!EWright_shift nil {:alone [:!EW#Pnon_us_pound]}]                                    ; menu search.
+    ; esc
+    ^{:doc/actions [{:program c/alf,   :action "menu search"}]}                                    [:#Pescape :!EWright_shift nil {:alone [:!EW#Pnon_us_pound]}]
 
-  ; zero
-    [:#Pkeypad_num_lock :!EQright_shift nil {:alone [:keypad_num_lock]}]                             ; mouse control
+    ; zero
+    ^{:doc/actions [{:program c/sys,   :action "mouse mode"}]}                                     [:#Pkeypad_num_lock :!EQright_shift nil {:alone [:keypad_num_lock]}]
 
-  ; launcher
-    [:#Ptab :!OTCright_shift nil {:alone :tab}]                                                      ; spectacle control, speak & app launcher
+    ; launcher
+    ^{:doc/actions [{:program c/sys,   :action "tab"}]}                                            [:#Ptab :!OTCright_shift nil {:alone :tab}]
 
-; TODO: annotate escape zellij
-  ; joker
-    [:##caps_lock :!EWright_command nil {:alone :escape}]                                            ; esc, language server protocol
+    ; joker
+    ^{:doc/actions [{:program c/hc,    :action "collapse selection", :exec hc-collapse}
+                    {:program c/zp,    :action "zellij escape",      :exec zp-escape}
+                    {:program c/zt,    :action "zellij escape",      :exec zt-escape}
+                    {:program c/zx,    :action "zellij escape",      :exec zx-escape}]}            [:##caps_lock :!EWright_command nil {:alone :escape}]
 
-; TODO: write an alone-key detector in babel interpret
-  ; grave
-    ; ^{:doc/actions [{:program c/hi, :action "mode normal", :exec hi-normal}
-    ;                 {:program c/hn, :action "mode insert", :exec hn-insert}
-    ;                 {:program c/hs, :action "mode normal", :exec hs-normal}]} [:#Pright_control :right_control nil {:alone [:f13]}]
+    ; grave
+    ^{:doc/actions [{:program c/hi,    :action "mode normal",        :exec hi-normal}
+                    {:program c/hn,    :action "mode insert",        :exec hn-insert}
+                    {:program c/hs,    :action "mode normal",        :exec hs-normal}]}            [c/kp_rt c/k_rt nil {:alone [:f13]}]
 
-    [:#Pright_control :right_control nil {:alone [:f13]}]
+    ; lefts
+    ^{:doc/actions [{:program c/alf,   :action "window switcher"}]}                                [c/kp_lc c/k_lc nil {:alone [:!EWQ#Onon_us_pound]}]
+    ^{:doc/actions [{:program c/sys,   :action "global in-app window cycler"}]}                    [c/kp_lo c/k_lo {:alone [:!Otab]}]
+    ^{:doc/actions [{:program c/sys,   :action "switch most recent app"}]}                         [c/kp_lc c/k_lc nil {:alone [:!Ctab]}]
 
-  ; lefts
-    [:#Pleft_control :left_control nil {:alone [:!EWQ#Onon_us_pound]}]                               ; alfred window switcher
-    [:#Pleft_option :left_option nil {:alone [:!Otab]}]                                              ; global in-app window cycler.
-    [:#Pleft_command :left_command nil {:alone [:!Ctab]}]                                            ; switch most recent app
-
-  ; rights
-    [:#Pright_shift :right_shift nil {:alone [:!R#Pnon_us_pound]}]                                   ; `browser` input. `term` history edit
-    [:#Pright_option :right_option nil {:alone [:!E#Pnon_us_pound]}]                                 ; alfred paste
-    [:#Pright_command :right_command nil {:alone [:!Q#Pnon_us_pound]}]                               ; alfred clipboard select
-    ]})
+    ; rights
+    ^{:doc/actions [{:program c/sys,   :action "browser input / term edit"}]}                      [c/kp_rs c/k_rs {:alone [:!R#Pnon_us_pound]}]
+    ^{:doc/actions [{:program c/alf,   :action "paste"}]}                                          [c/kp_ro c/k_ro nil {:alone [:!E#Pnon_us_pound]}]
+    ^{:doc/actions [{:program c/alf,   :action "clipboard select"}]}                               [c/kp_rc c/k_rc nil {:alone [:!Q#Pnon_us_pound]}]]})
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
