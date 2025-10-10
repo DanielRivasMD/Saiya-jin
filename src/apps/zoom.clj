c1a48ba383d0317378c01e8558961ca6a9cf5887
Daniel Rivas <danielrivasmd@gmail.com>
2025-10-10 11:34:17 +0300

    update zoom

--- src/apps/zoom.clj
+++ src/apps/zoom.clj
@@ -5,7 +5,7 @@
 (ns apps.zoom
   (:require [clojure.java.io :as io]
             [clojure.pprint :as pp]
-            [config :as c]))
+            [config.config :as c]))
 
 (def out-file "zoom.edn")
 
