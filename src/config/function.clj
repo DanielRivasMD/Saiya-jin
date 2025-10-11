8a17b2f2f008c5a4bbda60973c7d82b8d7f6afe0
Daniel Rivas <danielrivasmd@gmail.com>
2025-10-11 18:11:34 +0300

    add function config

---
+++ src/config/function.clj
@@ -0,0 +1,77 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; config
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(ns config.function
+(:require
+            [config.config :as c]))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+; function
+(def ks_f1  (c/mk c/K c/S c/_f1))
+(def ks_f2  (c/mk c/K c/S c/_f2))
+(def ks_f3  (c/mk c/K c/S c/_f3))
+(def ks_f4  (c/mk c/K c/S c/_f4))
+(def ks_f5  (c/mk c/K c/S c/_f5))
+(def ks_f6  (c/mk c/K c/S c/_f6))
+(def ks_f7  (c/mk c/K c/S c/_f7))
+(def ks_f8  (c/mk c/K c/S c/_f8))
+(def ks_f9  (c/mk c/K c/S c/_f9))
+(def ks_f10 (c/mk c/K c/S c/_f10))
+(def ks_f11 (c/mk c/K c/S c/_f11))
+(def ks_f12 (c/mk c/K c/S c/_f12))
+
+(def ko_f1  (c/mk c/K c/O c/_f1))
+(def ko_f2  (c/mk c/K c/O c/_f2))
+(def ko_f3  (c/mk c/K c/O c/_f3))
+(def ko_f4  (c/mk c/K c/O c/_f4))
+(def ko_f5  (c/mk c/K c/O c/_f5))
+(def ko_f6  (c/mk c/K c/O c/_f6))
+(def ko_f7  (c/mk c/K c/O c/_f7))
+(def ko_f8  (c/mk c/K c/O c/_f8))
+(def ko_f9  (c/mk c/K c/O c/_f9))
+(def ko_f10 (c/mk c/K c/O c/_f10))
+(def ko_f11 (c/mk c/K c/O c/_f11))
+(def ko_f12 (c/mk c/K c/O c/_f12))
+
+(def kt_f1  (c/mk c/K c/T c/_f1))
+(def kt_f2  (c/mk c/K c/T c/_f2))
+(def kt_f3  (c/mk c/K c/T c/_f3))
+(def kt_f4  (c/mk c/K c/T c/_f4))
+(def kt_f5  (c/mk c/K c/T c/_f5))
+(def kt_f6  (c/mk c/K c/T c/_f6))
+(def kt_f7  (c/mk c/K c/T c/_f7))
+(def kt_f8  (c/mk c/K c/T c/_f8))
+(def kt_f9  (c/mk c/K c/T c/_f9))
+(def kt_f10 (c/mk c/K c/T c/_f10))
+(def kt_f11 (c/mk c/K c/T c/_f11))
+(def kt_f12 (c/mk c/K c/T c/_f12))
+
+(def kts_f1  (c/mk c/K c/T c/S c/_f1))
+(def kts_f2  (c/mk c/K c/T c/S c/_f2))
+(def kts_f3  (c/mk c/K c/T c/S c/_f3))
+(def kts_f4  (c/mk c/K c/T c/S c/_f4))
+(def kts_f5  (c/mk c/K c/T c/S c/_f5))
+(def kts_f6  (c/mk c/K c/T c/S c/_f6))
+(def kts_f7  (c/mk c/K c/T c/S c/_f7))
+(def kts_f8  (c/mk c/K c/T c/S c/_f8))
+(def kts_f9  (c/mk c/K c/T c/S c/_f9))
+(def kts_f10 (c/mk c/K c/T c/S c/_f10))
+(def kts_f11 (c/mk c/K c/T c/S c/_f11))
+(def kts_f12 (c/mk c/K c/T c/S c/_f12))
+
+(def kos_f1  (c/mk c/K c/O c/S c/_f1))
+(def kos_f2  (c/mk c/K c/O c/S c/_f2))
+(def kos_f3  (c/mk c/K c/O c/S c/_f3))
+(def kos_f4  (c/mk c/K c/O c/S c/_f4))
+(def kos_f5  (c/mk c/K c/O c/S c/_f5))
+(def kos_f6  (c/mk c/K c/O c/S c/_f6))
+(def kos_f7  (c/mk c/K c/O c/S c/_f7))
+(def kos_f8  (c/mk c/K c/O c/S c/_f8))
+(def kos_f9  (c/mk c/K c/O c/S c/_f9))
+(def kos_f10 (c/mk c/K c/O c/S c/_f10))
+(def kos_f11 (c/mk c/K c/O c/S c/_f11))
+(def kos_f12 (c/mk c/K c/O c/S c/_f12))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
