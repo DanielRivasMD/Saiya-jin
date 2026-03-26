99835f94349b0c1f0d67d2f7de9ab11e6290d88a
Daniel Rivas <danielrivasmd@gmail.com>
2026-03-26 09:05:02 +0200

    update shift

--- src/simple/rshift.clj
+++ src/simple/rshift.clj
@@ -16,6 +16,16 @@
 
 (def out-file "rshift.edn")
 
+(def hc-goto-line            ["goto_line"])
+(def hc-goto-column          ["goto_column"])
+(def hc-last-mod             ["goto_last_modification"])
+
+(def hc-del-char-back        ["delete_char_backward"])
+(def hc-del-word-back        ["delete_word_backward"])
+(def mc-del-word-back        ["DeleteWordLeft"])
+(def hc-del-start-line       ["kill_to_line_start"])
+(def mc-del-start-line       ["SelectToStartOfLine,Delete"])
+
 (defn rshift []
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -23,21 +33,20 @@
   {:des "Shift Mode"
    :rules
    [;
-  ; [:!R#Pleft_arrow []]
-  ; [:!R#Pright_arrow []]
-  ; [:!R#Pup_arrow []]
-  ; [:!R#Pdown_arrow []]
+    ^{:doc/actions [{}]}                                                                           [r/krp_al      [r/kr_al]]
+    ^{:doc/actions [{}]}                                                                           [r/krp_ar      [r/kr_ar]]
+    ^{:doc/actions [{}]}                                                                           [r/krp_au      [r/kr_au]]
+    ^{:doc/actions [{}]}                                                                           [r/krp_ad      [r/kr_ad]]
 
-    ^{:doc/actions [{:program c/hc,    :action "delete line start", :exec ["kill_to_line_start"]}
-                    {:program c/mc,    :action "delete line start", :exec ["SelectToStartOfLine,Delete"]}]} [:!R#Pleft_command [:!Wu]]
-  ; [:!Rleft_option []]
-    ^{:doc/actions  [{:program c/hc,   :action "delete prev word", :exec ["delete_word_backward"]}
-                     {:program c/mc,   :action "delete prev word", :exec ["DeleteWordLeft"]}]} [:!R#Pleft_control [:!Ww]]
-    ^{:doc/actions [{:program c/hc,    :action "delete prev char", :exec ["delete_char_backward"]}]} [:!R#Pleft_shift [:delete_or_backspace]]                                                         ;
-  ; [:!R#Pcaps_lock []]
-  ; [:!R#Ptab []]
-  ; [:!R#Pkeypad_num_lock []]
-  ; [:!R#Pescape []]
+    ^{:doc/actions [{:program c/hc,    :action "last modification",  :exec hc-last-mod}]}          [a/krp_sp      [f/ko_f17]]
+    ^{:doc/actions [{:program c/hc,    :action "delete line start",  :exec hc-del-start-line}
+                    {:program c/mc,    :action "delete line start",  :exec mc-del-start-line}]}    [a/krp_lc      [c/kw_u]]
+    ^{:doc/actions [{:program c/hc,    :action "goto column",        :exec hc-goto-column}]}       [a/krp_lo      [f/ks_f4]]
+    ^{:doc/actions  [{:program c/hc,   :action "delete prev word",   :exec hc-del-word-back}
+                     {:program c/mc,   :action "delete prev word",   :exec mc-del-word-back}]}     [a/krp_lt      [c/kw_w]]
+    ^{:doc/actions [{:program c/hc,    :action "delete prev char",   :exec hc-del-char-back}]}     [a/krp_ls      [c/k_delb]]
+    ^{:doc/actions [{:program c/hc,    :action "go to column",       :exec hc-goto-line}]}         [a/krp_esc     [f/ks_f5]]
+    ^{:doc/actions [{}]}                                                                           [a/krp_tab     [a/kr_tab]]
     ]})
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
