1fd085c842da5212e64f76140af8a462ae434cf0
Daniel Rivas <danielrivasmd@gmail.com>
2026-04-05 18:49:00 +0200

    update keyboards

--- src/simple/keyboards.clj
+++ src/simple/keyboards.clj
@@ -13,20 +13,17 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-  {:des "Additional keys - MacPro"
+  ; TODO: update documentation
+  {:des "Additional keys"
    :rules
    [;
     ; keys - MacPro
     [:#Pdelete_or_backspace [:grave_accent_and_tilde] :pro]                                               ; '`'
-    [:!C#Pdelete_or_backspace [:!Sgrave_accent_and_tilde] :pro]                                           ; '~'
-    [:!O#Pdelete_or_backspace [:spacebar :!Sgrave_accent_and_tilde :spacebar] :pro]                       ; ' ~ '
+    [:!S#Pdelete_or_backspace [:!Sgrave_accent_and_tilde] :pro]                                           ; '~'
 
     ; keys - MacAir
     [:#Pdelete_or_backspace [:grave_accent_and_tilde] :air]                                               ; '`'
-    [:!T#Pdelete_or_backspace [:grave_accent_and_tilde :grave_accent_and_tilde :grave_accent_and_tilde] :air]  ; '```'
     [:!S#Pdelete_or_backspace [:!Sgrave_accent_and_tilde] :air]                                           ; '~'
-    [:!O#Pdelete_or_backspace [:spacebar :!Sgrave_accent_and_tilde :spacebar] :air]                       ; ' ~ '
-    [:!C#Pdelete_or_backspace [:spacebar :equal_sign :!Sgrave_accent_and_tilde :spacebar] :air]          ; ' =~ '
     [:f1 :display_brightness_decrement :air]
     [:f2 :display_brightness_increment :air]
     [:f3 :illumination_decrement :air]
@@ -38,7 +35,6 @@
     ; keys - Master
     [:#Pdelete_or_backspace [:grave_accent_and_tilde] :master]                                               ; '`'
     [:!S#Pdelete_or_backspace [:!Sgrave_accent_and_tilde] :master]                                           ; '~'
-    [:!O#Pdelete_or_backspace [:spacebar :!Sgrave_accent_and_tilde :spacebar] :master]                       ; ' ~ '
 
     ; keypad
     [:#Pkeypad_hyphen :mute]
