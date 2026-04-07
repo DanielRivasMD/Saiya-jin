be2cd0aba18452a7e80f2cc35948586a2e8f3791
Daniel Rivas <danielrivasmd@gmail.com>
2026-04-07 11:40:55 +0200

    update shift

--- src/simple/rshift.clj
+++ src/simple/rshift.clj
@@ -16,8 +16,6 @@
 
 (def out-file "rshift.edn")
 
-(def hc-goto-line            ["goto_line"])
-(def hc-goto-column          ["goto_column"])
 (def hc-last-mod             ["goto_last_modification"])
 (def hc-del-char-back        ["delete_char_backward"])
 (def hc-del-word-back        ["delete_word_backward"])
@@ -53,11 +51,11 @@
     ^{:doc/actions [{:program c/hc,    :action "last modification",  :exec hc-last-mod}]}          [a/krp_sp      [f/ko_f17]]
     ^{:doc/actions [{:program c/hc,    :action "delete line start",  :exec hc-del-start-line}
                     {:program c/mc,    :action "delete line start",  :exec mc-del-start-line}]}    [a/krp_lc      [c/kw_u]]
-    ^{:doc/actions [{:program c/hc,    :action "goto column",        :exec hc-goto-column}]}       [a/krp_lo      [f/ks_f4]]
-    ^{:doc/actions [{:program c/hc,   :action "delete prev word",    :exec hc-del-word-back}
-                    {:program c/mc,   :action "delete prev word",    :exec mc-del-word-back}]}     [a/krp_lt      [c/kw_w]]
+    ^{:doc/actions [{}]}                                                                           [a/krp_lo      [a/kr_lo]]
+    ^{:doc/actions [{:program c/hc,    :action "delete prev word",   :exec hc-del-word-back}
+                    {:program c/mc,    :action "delete prev word",   :exec mc-del-word-back}]}     [a/krp_lt      [c/kw_w]]
     ^{:doc/actions [{:program c/hc,    :action "delete prev char",   :exec hc-del-char-back}]}     [a/krp_ls      [c/k_delb]]
-    ^{:doc/actions [{:program c/hc,    :action "go to column",       :exec hc-goto-line}]}         [a/krp_caps    [f/ks_f5]]
+    ^{:doc/actions [{}]}                                                                           [a/krp_caps    [a/kr_caps]]
     ^{:doc/actions [{}]}                                                                           [a/krp_tab     [a/kr_tab]]
 
     ; numeric-glyphs
