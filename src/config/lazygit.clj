e86afa9e241ca8d24771abf1a6308cc2ba675672
Daniel Rivas <danielrivasmd@gmail.com>
2026-05-10 17:46:50 +0200

    add lazygit config

---
+++ src/config/lazygit.clj
@@ -0,0 +1,25 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(ns config.lazygit)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(def top                     ["gotoTop"])
+(def bottom                  ["gotoBottom"])
+(def scroll-up               ["scrollUpMain-alt2"])
+(def scroll-down             ["scrollDownMain-alt2"])
+
+(def prev-page               ["prevPage"])
+(def next-page               ["nextPage"])
+
+(def recent                  ["openRecentRepos"])
+(def commit                  ["commitChanges"])
+(def inc-content             ["increaseContextInDiffView"])
+(def dec-content             ["decreaseContextInDiffView"])
+(def next-block              ["nextBlock-alt2"])
+(def next-tab                ["nextTab"])
+(def prev-block              ["prevBlock-alt2"])
+(def prev-tab                ["prevTab"])
+(def zoom                    ["nextScreenMode"])
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
